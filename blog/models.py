7880e7cb7d43b6c0f3bbf110f7d17474df15c214
smin_516 <russoll.com>
2021-11-26 06:28:27 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,8 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -60,3 +62,16 @@ class Post(models.Model):
 
     def get_content_markdown(self):
         return markdown(self.content)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
